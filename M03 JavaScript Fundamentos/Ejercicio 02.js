5482bc379e6e612ff245ea75006e8203f07558c1
egt2000 <egt2000@gmail.com>
2023-08-23 06:55:19 -0300

    Commit M03 ejercicios 1 2 y 3

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,33 +4,50 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   
+   return(string);
 }
+console.log (devolverString);
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+
+   return( x + y );
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+
+   return( x - y );
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
-}
+   //x = 6;
+   //y = 3; 
 
+   return( x / y );
+}
+//console.log(divide);
+
+//var x = 6;
+//var y = 3;
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   return( x * y);
 }
+//console.log(multiplica);
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   return( x % y);
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
